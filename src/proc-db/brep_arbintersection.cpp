4f22c2d46b51338172f0fdce5d7a8bfb5806058c
李刚毅 <gregoryli@foxmail.com>
2022-06-29 16:35:21 +0800

    create brep test cases with arb8 intersection

---
+++ src/proc-db/brep_arbintersection.cpp
@@ -0,0 +1,205 @@
+/*                 B R E P _ S I M P L E . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2004-2022 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file proc-db/brep_arbintersection.cpp
+ *
+ * Creates a brep with the following topology making direct use of
+ * the openNURBS API:
+ *
+ *             H-------e6-------G
+ *            /                /|
+ *           / |              / |
+ *          /  e7            /  e5
+ *         /   |            /   |
+ *        /                e10  |
+ *       /     |          /     |
+ *      e11    E- - e4- -/- - - F
+ *     /                /      /
+ *    /      /         /      /
+ *   D---------e2-----C      e9
+ *   |     /          |     /
+ *   |    e8          |    /
+ *   e3  /            e1  /
+ *   |                |  /
+ *   | /              | /
+ *   |                |/
+ *   A-------e0-------B
+ *
+ * Copied almost verbatim from the file
+ * src/other/openNURBS/example_brep.cpp in order to explore the API
+ * for creating B-Reps. There is a slight bit more relevant
+ * documentation in this version than the other. In addition, this
+ * version uses the wdb interface to add the B-Rep to a BRL-CAD
+ * geometry file.
+ */
+
+#include "common.h"
+
+#include "twistedcube.h"
+
+#include "bio.h"
+
+#include "vmath.h"		/* BRL-CAD Vector macros */
+
+#include "bu/app.h"
+#include "wdb.h"
+#include "brep.h"
+#include "brep_arbintersection.h"
+#include "rt/geom.h"
+#include <string>
+
+struct rt_wdb* outfp;
+const char* db_name = "brep_arbintersection.g";
+
+static void
+printusage(void) {
+    fprintf(stderr,"Usage: brep_simple (takes no arguments)\n");
+}
+
+
+
+
+int
+com_add(
+    struct rt_wdb* wdbp,
+    const char* combname,
+    int size,
+    std::string* names,
+    const char* shadername,
+    const char* shaderargs,
+    const unsigned char* rgb)
+{
+    struct bu_list head;
+    BU_LIST_INIT(&head);
+    for (int i = 0; i < size; i++)
+    {
+        printf(names[i].data());
+        if (mk_addmember(names[i].data(), &head, NULL, WMOP_UNION) == WMEMBER_NULL)
+            return -2;
+    }
+    return mk_comb(wdbp, combname, &head, 1, shadername, shaderargs,
+        rgb, 0, 0, 0, 0, 0, 0, 0);
+}
+
+int test_UNION(rt_wdb* out, int arb_num, rt_arb_internal** arbs, ON_3dPoint points[][8], char* namehead)
+{
+    std::string head = namehead;
+    printf(std::to_string(arb_num).data());
+    std::string* names = new std::string[arb_num];for (int i = 0; i < arb_num; i++) {
+        BU_ALLOC(arbs[i], struct rt_arb_internal);
+        arbs[i]->magic = RT_ARB_INTERNAL_MAGIC;
+        for (int j = 0; j < 8; j++) {
+            VMOVE(arbs[i]->pt[j], points[i][j]);
+        }
+        std::string str= std::to_string(i);
+        names[i] = head + std::to_string(i);
+        wdb_export(out, names[i].data(), (void*)arbs[i], ID_ARB8, mk_conv2mm);
+    }
+
+
+    unsigned char rgb[] = { 255, 255, 255 };
+    std::string com_name = (head + ".r");
+    com_add(outfp, com_name.data(), arb_num, names, "plastic", "", rgb);
+
+
+
+    ON_Brep* brep = ON_Brep::New();
+    struct rt_db_internal brep_db_internal;
+
+    struct db_i* dbip = db_open(db_name, DB_OPEN_READONLY);
+    if (!dbip) {
+        bu_exit(1, "Unable to open brep_arbintersection.g geometry database file\n");
+    }
+    db_dirbuild(dbip);
+    struct directory* dirp;
+    if ((dirp = db_lookup(dbip, com_name.data(), 0)) != RT_DIR_NULL) {
+        printf("\tfound cube\n");
+        struct rt_db_internal ip;
+        mat_t mat;
+        MAT_IDN(mat);
+        if (rt_db_get_internal(&ip, dirp, dbip, mat, &rt_uniresource) >= 0) {
+
+        }
+        else {
+            fprintf(stderr, "problem getting internal object rep\n");
+        }
+
+        printf("brep!\n");
+        struct bn_tol tol;
+        tol.magic = BN_TOL_MAGIC;
+        tol.dist = BN_TOL_DIST;
+        tol.dist_sq = tol.dist * tol.dist;
+        tol.perp = SMALL_FASTF;
+        tol.para = 1.0 - tol.perp;
+        rt_comb_brep(&brep, &ip, &tol, dbip);
+
+        std::string brep_name = ("brep." + head);
+        mk_brep(outfp, brep_name.data(), (void*)brep);
+    }
+    return 0;
+}
+
+int
+main(int argc, char** argv)
+{
+    ON_TextLog error_log;
+    const char* id_name = "B-Rep Example";
+    const char* geom_name = "cube.s";
+
+    bu_setprogname(argv[0]);
+
+    if (BU_STR_EQUAL(argv[1], "-h") || BU_STR_EQUAL(argv[1], "-?")) {
+    	printusage();
+    	return 0;
+    }
+    if (argc > 1) {
+    	printusage();
+	return 1;
+    }
+
+    ON::Begin();
+
+    outfp = wdb_fopen(db_name);
+    mk_id(outfp, id_name);
+
+    test_UNION(outfp, arb_1_num, arb_1, arb_1_v, "arb_1");
+    test_UNION(outfp, arb_2_num, arb_2, arb_2_v, "arb_2");
+    test_UNION(outfp, arb_3_num, arb_3, arb_3_v, "arb_3");
+    test_UNION(outfp, arb_4_num, arb_4, arb_4_v, "arb_4");
+
+    wdb_close(outfp);
+    
+
+
+    ON::End();
+
+    return 0;
+}
+
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C++
+ * c-basic-offset: 4
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
