ef7e30804c11a4b22fcac11a43f39d755c1175b2
Gregory Li <gregoryli@foxmail.com>
2022-09-15 15:20:34 +0800

    erase processing of dplot_erase_overlay

--- src/libged/brep/dplot.c
+++ src/libged/brep/dplot.c
@@ -218,10 +218,12 @@ dplot_ssx_events(
     int ret;
 
     /* erase old event plots */
-    ret = dplot_erase_overlay(info, "curr_event");
+    /*ret = dplot_erase_overlay(info, "curr_event");
     if (ret != BRLCAD_OK) {
 	return ret;
-    }
+    }*/
+
+	dplot_erase_overlay(info, "curr_event");
 
     if (info->mode != DPLOT_SSX_EVENTS) {
 	return BRLCAD_OK;
